444f4f6a6b160d7b3d0d4a51aff471ceb6fa1550
jeonghoon <hnjh12195@naver.com>
2025-11-27 11:02:22 +0900

    초기파일

---
+++ src/test/java/com/abc/gemini/GeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.gemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
